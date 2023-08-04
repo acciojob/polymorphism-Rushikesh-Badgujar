6aae409c7344599f5270723a430f33dbefc92626
Rushikesh-Badgujar <rushikeshbadgujar1000@gmail.com>
2023-08-04 13:23:26 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,12 +12,12 @@ public class Main {
             return x * y;
         }
     }
-    public static void main(String args[] ){
-        Product p = new Produc();
+    public static void main(String [] args ){
+        Product p = new Product();
         int result0 = p.product(2,4);
         System.out.println("result0:-" +result0);
         int result1 = p.product(2,4,5);
-        System.out.println("result1 :-"+result);
+        System.out.println("result1 :-"+result1);
         double result2 = p.product(2.56,4.987);
         System.out.println("result2 :-"+result2);
     }
